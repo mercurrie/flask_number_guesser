ff06441904ea8b1a8fc74314289c0c9e8c79f91e
Mason C <43713560+mercurrie@users.noreply.github.com>
2023-02-16 18:19:50 -0600

    refactor
    included json file to hold styling and img data.
    utilized json data in main.py to reduce repetition.

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from flask import Flask
 import random
+import json
 
 app = Flask(__name__)
 random_number = random.randint(0, 9)
+data_file = open('data.json')
+DATA = json.load(data_file)
 
 
 @app.route('/')
 def home():
     return '<h1>Guess a number between 0 and 9!</h1>' \
            '<h3>Type a "/" after the address in your search bar, followed by your guess.</h3>' \
-           '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
+           f'<img src={DATA["HOME_IMG"]}>'
 
 
 @app.route("/<int:guess>")
 def guess_number(guess):
     if guess > random_number:
-        return '<h1 style="color: red">Too high, guess again!</h1>' \
-               '<img src="https://media.giphy.com/media/5noddpPJQxDNDaPWaP/giphy.gif">'
+        return f'<h1 style={DATA["HIGH_COLOR"]}>Too high, guess again!</h1>' \
+               f'<img src={DATA["HIGH_IMG"]}>'
     elif guess < random_number:
-        return '<h1 style="color: blue">Too low, guess again!</h1>' \
-               '<img src="https://media.giphy.com/media/B0AXfj1DdyoBAT6Ehn/giphy.gif">'
+        return f'<h1 style={DATA["LOW_COLOR"]}>Too low, guess again!</h1>' \
+               f'<img src={DATA["LOW_IMG"]}>'
 
     else:
-        return '<h1 style="color: green">You guessed correctly!!</h1>' \
-               '<img src="https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif">'
+        return f'<h1 style={DATA["CORRECT_COLOR"]}>You guessed correctly!!</h1>' \
+               f'<img src={DATA["CORRECT_IMG"]}>'
 
 
 if __name__ == "__main__":
